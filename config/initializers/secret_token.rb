cfc88ce9fa35ca463cc7c0424fc2d453a8a0d6ad
inkgab <keithar123@gmail.com>
2014-02-06 19:43:00 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Neeraj::Application.config.secret_key_base = 'a7c92d5cd42575b66ab4e0c5e490f823f66dbf475789eb0c8582b3bd6821afe90760d85f6918ee39cdfacc592a80a2f83ef50359e1a74a829e95aaac50ff9bb6'
